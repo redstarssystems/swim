1e1b69b0383b0922bdef28320b72435bec320c12
Mike Ananev <mike1452@yandex.ru>
2022-09-16 09:23:52 +0300

    events refactoring
    Took 2 hours 10 minutes

--- src/org/rssys/event.clj
+++ src/org/rssys/event.clj
@@ -21,10 +21,9 @@
    :probe-ack        10
    :stop             11
    :unknown          12
-   :new-cluster-size 13})
-
-
-(def ping-types {:direct 0 :indirect 1})
+   :new-cluster-size 13
+   :indirect-ping    14
+   :indirect-ack     15})
 
 
 (defprotocol ISwimEvent
@@ -32,21 +31,96 @@
   (restore [this v] "Restore Event from vector of values"))
 
 
-(defrecord PingEvent [cmd-type id host port restart-counter tx neighbour-id attempt-number ptype]
+
+(defrecord ProbeEvent [cmd-type id host port restart-counter tx neighbour-host neighbour-port probe-key]
+
+           ISwimEvent
+
+           (prepare [^ProbeEvent e]
+             [(.-cmd_type e) (.-id e) (.-host e) (.-port e) (.-restart_counter e)
+              (.-tx e) (.-neighbour_host e) (.-neighbour_port e) (.-probe_key e)])
+
+           (restore [^ProbeEvent _ v]
+             (if (and
+                   (vector? v)
+                   (= 9 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:probe code)) uuid? string? pos-int? nat-int?
+                                                nat-int? string? pos-int? any?] v)))
+               (apply ->ProbeEvent v)
+               (throw (ex-info "ProbeEvent vector has invalid structure" {:probe-vec v})))))
+
+
+
+(defn empty-probe
+  "Returns empty probe event"
+  ^ProbeEvent []
+  (map->ProbeEvent {:cmd-type        (:probe code)
+                    :id              (UUID. 0 0)
+                    :host            "localhost"
+                    :port            1
+                    :restart-counter 0
+                    :tx              0
+                    :neighbour-host  "localhost"
+                    :neighbour-port  1
+                    :probe-key       "probekey0"}))
+
+
+;;;;
+
+
+(defrecord ProbeAckEvent [cmd-type id host port status restart-counter tx neighbour-id probe-key]
+
+           ISwimEvent
+
+           (prepare [^ProbeAckEvent e]
+             [(.-cmd_type e) (.-id e) (.-host e) (.-port e) ((.-status e) code) (.-restart_counter e)
+              (.tx e) (.-neighbour_id e) (.-probe_key e)])
+
+           (restore [^ProbeAckEvent _ v]
+             (if (and
+                   (vector? v)
+                   (= 9 (count v))
+                   (let [status-pos 4]
+                     (every? true? (map #(%1 %2)
+                                     [#(= % (:probe-ack code)) uuid? string? pos-int? nat-int?
+                                      nat-int? nat-int? uuid? any?] v))
+                     (apply ->ProbeAckEvent (assoc v status-pos (get (clojure.set/map-invert code) (nth v status-pos))))))
+               (throw (ex-info "ProbeAckEvent vector has invalid structure" {:probe-ack-vec v})))))
+
+
+
+(defn empty-probe-ack
+  "Returns empty probe ack event"
+  ^ProbeAckEvent []
+  (map->ProbeAckEvent {:cmd-type        (:probe-ack code)
+                       :id              (UUID. 0 0)
+                       :host            "localhost"
+                       :port            1
+                       :status          :unknown
+                       :restart-counter 0
+                       :tx              0
+                       :neighbour-id    (UUID. 0 0)
+                       :probe-key       "probekey0"}))
+
+
+;;;;
+
+
+(defrecord PingEvent [cmd-type id host port restart-counter tx neighbour-id attempt-number]
 
            ISwimEvent
 
            (prepare [^PingEvent e]
              [(.-cmd_type e) (.-id e) (.-host e) (.-port e) (.-restart_counter e) (.-tx e)
-              (.-neighbour_id e) (.-attempt_number e) ((.-ptype e) ping-types)])
+              (.-neighbour_id e) (.-attempt_number e)])
 
            (restore [^PingEvent _ v]
              (if (and
                    (vector? v)
-                   (= 9 (count v))
+                   (= 8 (count v))
                    (every? true? (map #(%1 %2) [#(= % (:ping code)) uuid? string? pos-int? nat-int?
-                                                nat-int? uuid? pos-int? nat-int?] v)))
-               (apply ->PingEvent (assoc v 8 (get (clojure.set/map-invert ping-types) (nth v 8))))
+                                                nat-int? uuid? pos-int?] v)))
+               (apply ->PingEvent v)
                (throw (ex-info "PingEvent vector has invalid structure" {:ping-vec v})))))
 
 
@@ -59,25 +133,24 @@
                    :port            1
                    :restart-counter 0
                    :tx              0
-                   :neighbour-id    (UUID. 0 0)
-                   :attempt-number  1
-                   :ptype           :direct}))
-
+                   :neighbour-id    (UUID. 0 1)
+                   :attempt-number  1}))
 
 ;;;;
 
-(defrecord AckEvent [cmd-type id restart-counter tx neighbour-id neighbour-tx]
+(defrecord AckEvent [cmd-type id restart-counter tx neighbour-id neighbour-tx attempt-number]
 
            ISwimEvent
 
            (prepare [^AckEvent e]
-             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e) (.-neighbour_id e) (.-neighbour_tx e)])
+             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e)
+              (.-neighbour_id e) (.-neighbour_tx e) (.-attempt_number e)])
 
            (restore [^AckEvent _ v]
              (if (and
                    (vector? v)
-                   (= 6 (count v))
-                   (every? true? (map #(%1 %2) [#(= % (:ack code)) uuid? nat-int? nat-int? uuid? nat-int?] v)))
+                   (= 7 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:ack code)) uuid? nat-int? nat-int? uuid? nat-int? pos-int?] v)))
                (apply ->AckEvent v)
                (throw (ex-info "AckEvent vector has invalid structure" {:ack-vec v})))))
 
@@ -89,152 +162,122 @@
                   :id              (UUID. 0 0)
                   :restart-counter 0
                   :tx              0
-                  :neighbour-id    (UUID. 0 0)
-                  :neighbour-tx    0}))
+                  :neighbour-id    (UUID. 0 1)
+                  :neighbour-tx    0
+                  :attempt-number  1}))
 
 
 ;;;;
 
-
-(defrecord DeadEvent [cmd-type id restart-counter tx neighbour-id neighbour-tx]
+(defrecord IndirectPingEvent [cmd-type id host port restart-counter tx
+                              intermediate-id intermediate-host intermediate-port
+                              neighbour-id neighbour-host neighbour-port
+                              attempt-number]
 
            ISwimEvent
 
-           (prepare [^DeadEvent e]
-             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e) (.-neighbour_id e) (.-neighbour_tx e)])
+           (prepare [^IndirectPingEvent e]
+             [(.-cmd_type e) (.-id e) (.-host e) (.-port e) (.-restart_counter e) (.-tx e)
+              (.-intermediate_id e) (.-intermediate_host e) (.-intermediate_port e)
+              (.-neighbour_id e) (.-neighbour_host e) (.-neighbour_port e)
+              (.-attempt_number e)])
 
-           (restore [^DeadEvent _ v]
+           (restore [^IndirectPingEvent _ v]
              (if (and
                    (vector? v)
-                   (= 6 (count v))
-                   (every? true? (map #(%1 %2) [#(= % (:dead code)) uuid? nat-int? nat-int? uuid? nat-int?] v)))
-               (apply ->DeadEvent v)
-               (throw (ex-info "DeadEvent vector has invalid structure" {:dead-vec v})))))
+                   (= 13 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:indirect-ping code)) uuid? string? pos-int? nat-int? nat-int?
+                                                uuid? string? pos-int?
+                                                uuid? string? pos-int?
+                                                pos-int?] v)))
+               (apply ->IndirectPingEvent v)
+               (throw (ex-info "IndirectPingEvent vector has invalid structure" {:indirect-ping-vec v})))))
 
 
-(defn empty-dead
-  "Returns empty dead event"
-  ^DeadEvent []
-  (map->DeadEvent {:cmd-type        (:dead code)
-                   :id              (UUID. 0 0)
-                   :restart-counter 0
-                   :tx              0
-                   :neighbour-id    (UUID. 0 0)
-                   :neighbour-tx    0}))
+(defn empty-indirect-ping
+  "Returns empty indirect ping event"
+  ^IndirectPingEvent []
+  (map->IndirectPingEvent {:cmd-type          (:indirect-ping code)
+                           :id                (UUID. 0 0)
+                           :host              "localhost"
+                           :port              1
+                           :restart-counter   0
+                           :tx                0
+                           :intermediate-id   (UUID. 0 1)
+                           :intermediate-host "localhost"
+                           :intermediate-port 10
+                           :neighbour-id      (UUID. 0 2)
+                           :neighbour-host    "localhost"
+                           :neighbour-port    100
+                           :attempt-number    1}))
 
 
 ;;;;
 
-
-(defrecord ProbeEvent [cmd-type id host port restart-counter tx neighbour-host neighbour-port]
+(defrecord IndirectAckEvent [cmd-type id restart-counter tx status host port
+                             intermediate-id intermediate-host intermediate-port
+                             neighbour-id neighbour-host neighbour-port
+                             attempt-number]
 
            ISwimEvent
 
-           (prepare [^ProbeEvent e]
-             [(.-cmd_type e) (.-id e) (.-host e) (.-port e) (.-restart_counter e) (.-tx e) (.-neighbour_host e) (.-neighbour_port e)])
+           (prepare [^IndirectAckEvent e]
+             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.-tx e)
+              (.-status e) (.-host e) (.-port e)
+              (.-intermediate_id e) (.-intermediate_host e) (.-intermediate_port e)
+              (.-neighbour_id e) (.-neighbour_host e) (.-neighbour_port e)
+              (.-attempt_number e)])
 
-           (restore [^ProbeEvent _ v]
+           (restore [^IndirectAckEvent _ v]
              (if (and
                    (vector? v)
-                   (= 8 (count v))
-                   (every? true? (map #(%1 %2) [#(= % (:probe code)) uuid? string? pos-int? nat-int? nat-int? string? pos-int?] v)))
-               (apply ->ProbeEvent v)
-               (throw (ex-info "ProbeEvent vector has invalid structure" {:probe-vec v})))))
+                   (= 14 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:indirect-ack code)) uuid? nat-int? nat-int?
+                                                nat-int? string? pos-int?
+                                                uuid? string? pos-int?
+                                                uuid? string? pos-int?
+                                                pos-int?] v)))
+               (apply ->IndirectAckEvent v)
+               (throw (ex-info "IndirectAckEvent vector has invalid structure" {:indirect-ping-vec v})))))
 
 
-
-(defn empty-probe
-  "Returns empty probe event"
-  ^ProbeEvent []
-  (map->ProbeEvent {:cmd-type        (:probe code)
-                    :id              (UUID. 0 0)
-                    :host            "localhost"
-                    :port            0
-                    :restart-counter 0
-                    :tx              0
-                    :neighbour-host  "localhost"
-                    :neighbour-port  0}))
-
-
-;;;;
-
-(defrecord ProbeAckEvent [cmd-type id host port status restart-counter tx neighbour-id neighbour-tx]
-
-           ISwimEvent
-
-           (prepare [^ProbeAckEvent e]
-             [(.-cmd_type e) (.-id e) (.-host e) (.-port e) ((.-status e) code) (.-restart_counter e)
-              (.tx e) (.-neighbour_id e) (.-neighbour_tx e)])
-
-           (restore [^ProbeAckEvent _ v]
-             (if (and
-                   (vector? v)
-                   (= 9 (count v))
-                   (every? true? (map #(%1 %2)
-                                   [#(= % (:probe-ack code)) uuid? string? nat-int? nat-int?
-                                    nat-int? nat-int? uuid? nat-int?] v)))
-               (apply ->ProbeAckEvent (assoc v 4 (get (clojure.set/map-invert code) (nth v 4))))
-               (throw (ex-info "ProbeAckEvent vector has invalid structure" {:probe-ack-vec v})))))
-
-
-
-(defn empty-probe-ack
-  "Returns empty probe ack event"
-  ^ProbeAckEvent []
-  (map->ProbeAckEvent {:cmd-type        (:probe-ack code)
-                       :id              (UUID. 0 0)
-                       :host            "127.0.0.1"
-                       :port            0
-                       :status          :unknown
-                       :restart-counter 0
-                       :tx              0
-                       :neighbour-id    (UUID. 0 0)
-                       :neighbour-tx    0}))
+(defn empty-indirect-ack
+  "Returns empty indirect ack event"
+  ^IndirectAckEvent []
+  (map->IndirectAckEvent {:cmd-type          (:indirect-ack code)
+                          :id                (UUID. 0 0)
+                          :restart-counter   0
+                          :tx                0
+                          :status            :alive
+                          :host              "localhost"
+                          :port              1
+                          :intermediate-id   (UUID. 0 1)
+                          :intermediate-host "localhost"
+                          :intermediate-port 10
+                          :neighbour-id      (UUID. 0 2)
+                          :neighbour-host    "localhost"
+                          :neighbour-port    100
+                          :attempt-number    1}))
 
 
 ;;;;
 
 
-(defrecord AntiEntropy [cmd-type id  restart-counter tx anti-entropy-data]
-           ISwimEvent
-
-           (prepare [^AntiEntropy e]
-             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.-tx e) (.-anti_entropy_data e)])
-
-           (restore [^AntiEntropy _ v]
-             (if (and
-                   (vector? v)
-                   (= 5 (count v))
-                   (every? true? (map #(%1 %2) [#(= % (:anti-entropy code)) uuid? nat-int? nat-int? vector?] v)))
-               (apply ->AntiEntropy v)
-               (throw (ex-info "AntiEntropy vector has invalid structure" {:anti-entropy-vec v})))))
-
-
-(defn empty-anti-entropy
-  "Returns empty anti-entropy event"
-  ^AntiEntropy []
-  (map->AntiEntropy {:cmd-type          (:anti-entropy code)
-                     :id                (UUID. 0 0)
-                     :restart-counter   0
-                     :tx                0
-                     :anti-entropy-data []}))
-
-
-;;;;
-
-
-(defrecord AliveEvent [cmd-type id restart-counter tx neighbour-id neighbour-tx]
+(defrecord AliveEvent [cmd-type id restart-counter tx
+                       neighbour-id neighbour-restart-counter neighbour-tx]
 
            ISwimEvent
 
            (prepare [^AliveEvent e]
-             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e) (.-neighbour_id e) (.-neighbour_tx e)])
+             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e)
+              (.-neighbour_id e) (.-neighbour_restart_counter e) (.-neighbour_tx e)])
 
            (restore [^AliveEvent _ v]
              (if (and
                    (vector? v)
-                   (= 6 (count v))
-                   (every? true? (map #(%1 %2) [#(= % (:alive code)) uuid? nat-int? nat-int? uuid? nat-int?] v)))
+                   (= 7 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:alive code)) uuid? nat-int? nat-int?
+                                                uuid? nat-int? nat-int?] v)))
                (apply ->AliveEvent v)
                (throw (ex-info "AliveEvent vector has invalid structure" {:alive-vec v})))))
 
@@ -242,13 +285,13 @@
 (defn empty-alive
   "Returns empty alive event"
   ^AliveEvent []
-  (map->AliveEvent {:cmd-type        (:alive code)
-                    :id              (UUID. 0 0)
-                    :restart-counter 0
-                    :tx              0
-                    :neighbour-id    (UUID. 0 0)
-                    :neighbour-tx    0}))
-
+  (map->AliveEvent {:cmd-type                  (:alive code)
+                    :id                        (UUID. 0 0)
+                    :restart-counter           0
+                    :tx                        0
+                    :neighbour-id              (UUID. 0 1)
+                    :neighbour-restart-counter 0
+                    :neighbour-tx              0}))
 
 
 ;;;;
@@ -276,5 +319,67 @@
                              :id               (UUID. 0 0)
                              :restart-counter  0
                              :tx               0
-                             :old-cluster-size 0
-                             :new-cluster-size 0}))
+                             :old-cluster-size 1
+                             :new-cluster-size 2}))
+
+;;;;
+
+
+(defrecord DeadEvent [cmd-type id restart-counter tx neighbour-id neighbour-restart-counter neighbour-tx]
+
+           ISwimEvent
+
+           (prepare [^DeadEvent e]
+             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.tx e) (.-neighbour_id e)
+              (.-neighbour_restart_counter e) (.-neighbour_tx e)])
+
+           (restore [^DeadEvent _ v]
+             (if (and
+                   (vector? v)
+                   (= 7 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:dead code)) uuid? nat-int? nat-int? uuid? nat-int? nat-int?] v)))
+               (apply ->DeadEvent v)
+               (throw (ex-info "DeadEvent vector has invalid structure" {:dead-vec v})))))
+
+
+(defn empty-dead
+  "Returns empty dead event"
+  ^DeadEvent []
+  (map->DeadEvent {:cmd-type                  (:dead code)
+                   :id                        (UUID. 0 0)
+                   :restart-counter           0
+                   :tx                        0
+                   :neighbour-id              (UUID. 0 1)
+                   :neighbour-restart-counter 0
+                   :neighbour-tx              0}))
+
+
+;;;;
+
+
+(defrecord AntiEntropy [cmd-type id restart-counter tx anti-entropy-data]
+           ISwimEvent
+
+           (prepare [^AntiEntropy e]
+             [(.-cmd_type e) (.-id e) (.-restart_counter e) (.-tx e) (.-anti_entropy_data e)])
+
+           (restore [^AntiEntropy _ v]
+             (if (and
+                   (vector? v)
+                   (= 5 (count v))
+                   (every? true? (map #(%1 %2) [#(= % (:anti-entropy code)) uuid? nat-int? nat-int? vector?] v)))
+               (apply ->AntiEntropy v)
+               (throw (ex-info "AntiEntropy vector has invalid structure" {:anti-entropy-vec v})))))
+
+
+(defn empty-anti-entropy
+  "Returns empty anti-entropy event"
+  ^AntiEntropy []
+  (map->AntiEntropy {:cmd-type          (:anti-entropy code)
+                     :id                (UUID. 0 0)
+                     :restart-counter   0
+                     :tx                0
+                     :anti-entropy-data []}))
+
+
+
