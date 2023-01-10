894601dd9e43b5ba87b5751c2cf57bc912cf98e4
Mike Ananev <mike1452@yandex.ru>
2023-01-10 21:53:03 +0300

    Add notification other neighbour on leaving the cluster

--- src/org/rssys/swim.clj
+++ src/org/rssys/swim.clj
@@ -1846,6 +1846,13 @@
   "Leave the cluster"
   [^NodeObject this]
   (stop-rejoin-watcher this)
+
+  (let [n (calc-n (nodes-in-cluster this))
+        neighbour-ids (take-ids-for-ping this n)
+        left-event (new-left-event this)]
+    (doseq [nb-id neighbour-ids]
+      (send-event this left-event nb-id)))
+
   (set-left-status this))
 
 
