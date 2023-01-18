2f823f1d0c6169093e3fbda3d9cc84408ba6d00c
Mike Ananev <mike1452@yandex.ru>
2023-01-18 10:37:52 +0300

    Add monitoring capabilities

---
+++ src/org/rssys/monitoring.clj
@@ -0,0 +1,8 @@
+(ns org.rssys.monitoring
+  (:require [prometheus.core :as prometheus]))
+
+(def registry (prometheus/new-registry))
+
+(prometheus/register-metric-meta registry :ping-ack-round-trip-ms :buckets [0 5 30 50 100 200 500 1000 2000 3000 10000 20000])
+
+
