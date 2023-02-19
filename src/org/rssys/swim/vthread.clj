44ad0b1c29b20d85cf3baf2a568e5520bd102a58
Mike Ananev <mike1452@yandex.ru>
2023-02-19 21:56:53 +0300

    vthread refactoring
    vthread was refactored for Babashka compatibility (branch jdk19-loom).
    Now Clojure agents will use loom threads cause we explicitly set them to unbounded executor.

--- src/org/rssys/swim/vthread.clj
+++ src/org/rssys/swim/vthread.clj
@@ -2,34 +2,17 @@
   (:import
     (java.util.concurrent
       ExecutorService
-      Executors
-      ThreadFactory)))
+      Executors)))
 
 
-(defn thread-factory
-  "Factory that helps with spawning new Virtual Threads"
-  ^ThreadFactory [^String name]
-  (-> (Thread/ofVirtual)
-    (.name name 0)
-    (.factory)))
-
-
-(defonce unbounded-executor (Executors/newThreadPerTaskExecutor (thread-factory "common-virtual-pool-")))
+(defonce unbounded-executor (Executors/newVirtualThreadPerTaskExecutor))
 
 (set-agent-send-executor! unbounded-executor)
-
 (set-agent-send-off-executor! unbounded-executor)
-
-
-(defmacro vfuture
-  "Takes a body of expressions and invoke the body in another virtual thread.
-  Returns ^java.util.concurrent.ThreadPerTaskExecutor$ThreadBoundFuture"
-  [& body]
-  `(.submit ^ExecutorService unbounded-executor ^Callable (^{:once true} fn* [] ~@body)))
-
 
 (defmacro vthread
   "Takes a body of expressions and invoke the body in another virtual thread."
   [& body]
-  `(Thread/startVirtualThread ^Runnable (^{:once true} fn* [] ~@body)))
+  `(.submit ^ExecutorService unbounded-executor ^Callable (^{:once true} fn* [] ~@body)))
+
 
