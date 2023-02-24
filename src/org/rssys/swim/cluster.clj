962253f72171b2306511dbdaa9c437a5b9475fa0
Mike Ananev <mike1452@yandex.ru>
2023-02-24 17:26:11 +0300

    Remove password from Cluster entity
    Now we generate secret key from password and never save password in Cluster.

--- src/org/rssys/swim/cluster.clj
+++ src/org/rssys/swim/cluster.clj
@@ -2,40 +2,24 @@
   (:require
     [clojure.spec.alpha :as s]
     [org.rssys.swim.encrypt :as e]
-    [org.rssys.swim.spec :as spec])
-  (:import (java.io Writer)))
+    [org.rssys.swim.spec :as spec]))
 
-
-(defn cluster-str
-  "Returns String representation of Cluster"
-  ^String
-  [cluster]
-  (str (into {} (assoc cluster :password "***censored***"))))
-
-
-(defrecord Cluster [id name desc password namespace tags secret-key cluster-size]
-           Object
-           (toString [this] (cluster-str this)))
-
-
-(defmethod print-method Cluster [cluster ^Writer writer]
-  (.write writer (cluster-str cluster)))
-
-
-(defmethod print-dup Cluster [cluster ^Writer writer]
-  (.write writer (cluster-str cluster)))
+(defrecord Cluster [id name desc namespace tags secret-key cluster-size])
 
 
 (defn new-cluster
   "Returns new Cluster instance."
-  ^Cluster [{:keys [id name password namespace desc cluster-size tags] :as c}]
-  (when-not (s/valid? ::spec/cluster c)
-    (throw (ex-info "Invalid cluster data" (->> c (s/explain-data ::spec/cluster) spec/problems))))
-  (map->Cluster {:id           (or id (random-uuid))
-                 :name         name
-                 :desc         (or desc "")
-                 :password     password
-                 :namespace    namespace
-                 :tags         (or tags #{})
-                 :secret-key   (e/gen-secret-key password)
-                 :cluster-size (or cluster-size 1)}))
+  ^Cluster [{:keys [id name password namespace desc cluster-size tags]}]
+  (when-not (s/valid? ::spec/password password)
+    (throw (ex-info "Invalid cluster password" (->> password (s/explain-data ::spec/password) spec/problems))))
+  (let [cluster (map->Cluster {:id           (or id (random-uuid))
+                               :name         name
+                               :desc         (or desc "")
+                               :namespace    namespace
+                               :tags         (or tags #{})
+                               :secret-key   (e/gen-secret-key password)
+                               :cluster-size (or cluster-size 1)})]
+
+    (if-not (s/valid? ::spec/cluster cluster)
+      (throw (ex-info "Invalid cluster data" (->> cluster (s/explain-data ::spec/cluster) spec/problems)))
+      cluster)))
--- test/org/rssys/swim/cluster_test.clj
+++ test/org/rssys/swim/cluster_test.clj
@@ -1,11 +1,12 @@
 (ns org.rssys.swim.cluster-test
   (:require [clojure.string :as string]
-            [clojure.test :refer [deftest testing]]
+            [clojure.test :refer [deftest is testing]]
             [matcho.core :as m]
             [org.rssys.swim.cluster :as sut]
             [org.rssys.swim.spec :as spec])
   (:import (org.rssys.swim.cluster Cluster)))
 
+(declare thrown-with-msg?)
 
 (def cluster-data
   {:id           #uuid "10000000-0000-0000-0000-000000000000"
@@ -21,9 +22,6 @@
   (testing "Create Cluster instance"
     (let [cluster (sut/new-cluster cluster-data)]
 
-      (testing "should prevent password serialization to string"
-        (m/assert #(string/includes? % ":password \"***censored***\"") (pr-str cluster)))
-
       (testing "should produce correct type"
         (m/assert Cluster (type cluster)))
 
@@ -31,4 +29,8 @@
         (m/assert ::spec/cluster cluster))
 
       (testing "should have expected cluster size"
-        (m/assert 3 (.-cluster_size cluster))))))
+        (m/assert 3 (.-cluster_size cluster)))
+
+      (testing "should prevent small or empty password"
+        (is (thrown-with-msg? Exception #"Invalid cluster password"
+              (sut/new-cluster {:password "abcde"})))))))
