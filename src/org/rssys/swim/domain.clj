ff74fc12833d61d35990e754eb9c362a6aa8c682
Mike Ananev <mike1452@yandex.ru>
2023-02-25 12:05:42 +0300

    Remove node object from ns domain

--- src/org/rssys/swim/domain.clj
+++ src/org/rssys/swim/domain.clj
@@ -19,25 +19,3 @@
                           updated-at])
 
 
-(defrecord Node [id
-                 host
-                 port
-                 cluster
-                 status
-                 neighbours
-                 restart-counter
-                 tx
-                 ping-events
-                 indirect-ping-events
-                 payload
-                 *udp-server
-                 outgoing-events
-                 ping-round-buffer
-                 probe-events]
-           Object
-           (toString [this] (.toString this)))
-
-
-(defrecord NodeObject [*node])
-
-
